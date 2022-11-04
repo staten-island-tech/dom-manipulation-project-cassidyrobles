6fcc6f108e7c60579aca9a3e29c83cd905952bd3
cassidyrobles <cassidyrobles19@gmail.com>
2022-11-04 13:15:58 -0400

    help

---
+++ index.js
@@ -0,0 +1,6 @@
+const DOMselectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  point: document.querySelector(".point"),
+};
+console.log(DOMselectors.point);
