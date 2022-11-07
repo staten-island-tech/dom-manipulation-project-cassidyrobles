c4b56a7cde7826928b9cf11ad02d577c292fec15
cassidyrobles <cassidyrobles19@gmail.com>
2022-11-07 13:16:06 -0500

    hgygoh

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const DOMselectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
   point: document.querySelector(".point"),
+  box: document.getElementById("big-black-box"),
 };
-console.log(DOMselectors.point);
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.innerHTML = "this is now a big red box";
+}
+
+backgroundAndText(DOMselectors.box, DOMselectors.text);
