05a180fed663cc39a65cd66c521c348ae4acee2e
cassidyrobles <cassidyrobles19@gmail.com>
2022-11-21 21:37:11 -0500

    nonononono

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const DOMselector = {
-  button: document.querySelector(".btn"),
+const DOMselectors = {
+  btn: document.querySelector(".btn"),
   title: document.querySelector("#title"),
   artist: document.querySelector("#artist"),
   albumCover: document.querySelector("#url"),
   display: document.querySelector(".display"),
+  remove: document.querySelector(".remove"),
+  displayCard: document.querySelector(".display-card"),
 };
 
 function cardThing() {
-  let title = DOMSelectors.title.value;
-  let artist = DOMSelectors.artist.value;
-  let albumCover = DOMSelector.albumCover.value;
-  DOMselector.display.insertAdjacentHTML(
-    "afterend",
-    `<h2>${title}</h2> <h2>${artist}</h2> <h2>${albumCover}</h2>`
+  let title = DOMselectors.title.value;
+  let artist = DOMselectors.artist.value;
+  let albumCover = DOMselectors.albumCover.value;
+  console.log(title);
+  console.log(artist);
+  console.log(albumCover);
+  DOMselectors.display.insertAdjacentHTML(
+    "afterend", 
+    `<div class="display-card"> <img class="display-img" src="${albumCover}"/> <h2 class="display-artist">${artist}</h2> <h3 class="display-album">${title}</h3> <button class="remove">Remove Album</button> </div>`
   );
 }
 
-DOMSelectors.btn.addEventListener("click", cardThing);
+DOMselectors.btn.addEventListener("click", cardThing);
+DOMselectors.remove.addEventListener("click", removeCardThing);
+
+function removeCardThing(){
+let cardHTML = DOMselectors.displayCard;
+cardHTML.remove();
+}
