7dea082aa76d86533db40e3e940bb2b398e5ed39
cassidyrobles <cassidyrobles19@gmail.com>
2022-11-29 13:10:45 -0500

    YAY!!!

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-let count = 0;
-
 const DOMselectors = {
   btn: document.querySelector(".btn"),
   title: document.querySelector("#title"),
@@ -18,37 +15,40 @@ function cardThing() {
   console.log(title);
   console.log(artist);
   console.log(albumCover);
-  
-if(
-  title === "" || artist === "" || albumCover === ""
-){
-  alert("complete all fields");
-  return;
-}
+
+  if (title === "" || artist === "" || albumCover === "") {
+    alert("complete all fields");
+    return;
+  }
 
   DOMselectors.display.insertAdjacentHTML(
-    "afterend", 
-    `<div class= "display-card" id="display-card-${count}"> <img class="display-img" src="${albumCover}"/> <h2 class="display-artist">${artist}</h2> <h3 class="display-album">${title}</h3> <button id="btn-${count}">Remove Album</button> </div>`
-    );
-    document.querySelector(`#btn-${count}`).addEventListener("click", removeCardThing);
-    clear();
-  
-    count= count +1;
-  }
-  
-  DOMselectors.btn.addEventListener("click", cardThing);
-  
-  function removeCardThing(event){
-    let btn = event.target;
-    let counter = btn.id.substring(4);
-    document.querySelector(`#display-card-${counter}`).remove();
-    }
-    
-    function clear(){
-      let titleBox = DOMselectors.title;
-      let artistBox = DOMselectors.artist;
-      let albumCoverBox = DOMselectors.albumCover;
-      titleBox.value= "";
-      artistBox.value= "";
-      albumCoverBox.value= "";
-    }
+    "afterend",
+    `<div class= "display-card"> 
+    <img class="display-img" src="${albumCover}"/>
+     <h2 class="display-artist">${artist}</h2> 
+     <h3 class="display-album">${title}</h3> <button class ="remove">Remove Album</button> </div>`
+  );
+
+  clear();
+  removeCard();
+}
+
+DOMselectors.btn.addEventListener("click", cardThing);
+
+function clear() {
+  let titleBox = DOMselectors.title;
+  let artistBox = DOMselectors.artist;
+  let albumCoverBox = DOMselectors.albumCover;
+  titleBox.value = "";
+  artistBox.value = "";
+  albumCoverBox.value = "";
+}
+
+function removeCard() {
+  let buttons = document.querySelectorAll(".remove");
+  buttons.forEach((button) =>
+    button.addEventListener("click", function (event) {
+      event.target.parentElement.remove();
+    })
+  );
+}
